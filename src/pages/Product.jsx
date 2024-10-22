71a7f5c77c76778fccf43f4b4402c77197300477
Steven Jaime <steven@Stevens-MBP-2.attlocal.net>
2024-10-21 21:15:49 -0400

    update to image styles

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -36,27 +36,27 @@ const Product = () => {
     return (
 
         
-        <div style={{display: 'flex'}}>
-             
-            <div className="left-box" style={{display:'flex'}}>
+        <div className='container' style={{display: 'flex'}}>             
+            <div className="left-box" style={{width:'50%'}}> 
 
-                <div className="img-display">
-                    <img src={images[0]} style={{width:'500px', height:'500px'}}  />
+                <div className="img-display" style={{objectFit:'cover'}}>
+                    <img src={images[0]} style={{width:'100%'}}  />
                 </div>
 
                 <div className="img-gallery">
                     <ul>
                         {images.slice(1).map((a, idx) => (
-                            <li style={{ width: '40px', height: '40px'}} key={idx}><img style={{width:'100%'}} src={a} /></li>
+                            <li style={{objectFit:'cover'}} key={idx}><img style={{width:'100%'}} src={a} /></li>
                         ))}
                     </ul>
                 </div>
                 
             </div>
-            <div className="right-box">
-                <div className="item-detail">
+
+            <div className="right-box" style={{width:'50%'}}>
+                <div className="item-detail" style={{ display:'flex', justifyContent:'space-between'}}>
                     <div>
-                        <h2>{itemDetails[0].name}</h2>
+                        <h2 style={{ margin:'0' }}>{itemDetails[0].name}</h2>
                         <span>{itemDetails[0].whitePrice.price}</span>
                     </div>
                     <div>
@@ -64,7 +64,7 @@ const Product = () => {
                     </div>
                 </div>
 
-                <div className="item-color">
+                <div className="item-color" style={{margin:'10px 0'}}>
                     <span>color</span>
                     <span>img</span>
                 </div>
